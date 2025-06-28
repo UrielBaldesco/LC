eb67f8a28058e36f6776ec84cd7ea080cb9ecacb
uri <74332435+UrielBaldesco@users.noreply.github.com>
2025-06-28 15:55:30 -0700

    Time: 3 ms (38.06%) | Memory: 17.7 MB (99.32%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,16 +1,20 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        write, read = 0,0
+        read, write = 0,0
         while read < len(chars):
             letter = chars[read]
-            countOcc = 0
+            count = 0
             while read < len(chars) and chars[read] == letter:
-                countOcc += 1
                 read += 1
+                count += 1
             chars[write] = letter
             write += 1
-            if countOcc > 1:
-                for c in str(countOcc):
+            if count > 1:
+                for c in str(count):
                     chars[write] = c
                     write += 1
+        
         return write
+'''
+["a","a","b","b","c","c","c"]
+'''
