7a52995ad55a9fb740eda9bc2f99558f2bce1e7d
uri <74332435+UrielBaldesco@users.noreply.github.com>
2025-06-28 16:05:06 -0700

    Time: 3 ms (38.06%) | Memory: 18.1 MB (6.31%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,17 +1,20 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
         write, read = 0,0
+
         while read < len(chars):
             letter = chars[read]
             count = 0
             while read < len(chars) and chars[read] == letter:
                 read += 1
                 count += 1
+            
             chars[write] = letter
             write += 1
             if count > 1:
                 for c in str(count):
                     chars[write] = c
                     write += 1
-        
+            
         return write
+
