6b20a325048b53488ad6e4ea63939968b59818b1
uri <74332435+UrielBaldesco@users.noreply.github.com>
2025-11-29 15:09:38 -0800

    Time: 371 ms (5.05%) | Memory: 20.1 MB (6.22%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,9 +1,32 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_map = Counter(nums)
-        heap = []
-        for num, freq in freq_map.items():
-            heapq.heappush(heap, (freq,num))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return [num for freq, num in heap]
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        #2d array, each index in the array corresponds to the frequency and each element in the indx is a list. the list inside each index is the number that has that many frequencies 
+        
+        #step 1: have a dictionary that has a key that is the number and the value that is the frequency
+        #step 2: have a 2d array of lsits
+        bucket = [[] for _ in range(len(nums)+1)]
+        print(bucket)
+        
+        #step 3: populate count
+        count = Counter(nums)
+        print(count)
+
+        for key,val in count.items():
+            bucket[val].append(key)
+        print(bucket)
+        res = []
+        #step 4:iterate backwards the bucket array and append the numbers to the result until k is 0
+        for i in range(len(bucket)-1,0,-1):
+            for sublist in bucket[i]:
+                res.append(sublist)
+                if len(res) == k:
+                    return res
+        
+
+        
+            
