92c807cf28eb2387fd64ddb4bd2c27f7f58e16bd
uri <74332435+UrielBaldesco@users.noreply.github.com>
2025-11-29 22:18:56 -0800

    Time: 8 ms (72.84%) | Memory: 14.3 MB (55.71%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,20 +6,12 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        '''
-        heap where each node is a tuple 
-            the tuple is (the number, the frequency)
-        
-        step 1: create a frequency map
-        step 2: create a heap list 
-        step 3: populate the heap, restrict the heap to length of k
-        '''
+        #create a map with key as number, val is the freq
         count = Counter(nums)
-        heap = []
-        for key, val in count.items():
-            heapq.heappush(heap,(val,key)) #the frequency should be change the order
+        heap = [] #heap is a list with an order
+        for key,val in count.items(): #iterate the key,vals in the map
+            heapq.heappush(heap, (val,key)) #populate the heap
             if len(heap) > k:
                 heapq.heappop(heap)
-        print(heap)
-        return [key for (val, key) in heap]
- #iterates through each tuple, ignores the first index in the tuple and only care about the second index of the tuple -- aka the actual number, the first index is the frequency which we dont want
+        return [key for (val,key) in heap]
+
